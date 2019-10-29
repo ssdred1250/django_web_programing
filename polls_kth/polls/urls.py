eb662dbcfb027da3b574cd59548e49322d6ce2c2
kim tae hyung <>
2019-10-29 15:33:48 +0900

    no message

--- polls_kth/polls/urls.py
+++ polls_kth/polls/urls.py
@@ -5,5 +5,6 @@ from .views import *
 urlpatterns = [
     path('', index, name='index'),
     path('<int:question_id>/', detail, name='detail'),
-    path('<int:question_id>/result', result, name='result')
+    path('<int:question_id>/result', result, name='result'),
+    path('<int:question_id>/vote', vote, name='vote')
 ]
--- polls_kth/polls/views.py
+++ polls_kth/polls/views.py
@@ -1,14 +1,34 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, get_list_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import *
 
 
 def index(request):
-    return HttpResponse('투표 목록입니다.')
+    latest_question_list = Question.objects.order_by('pub_data')
+    context = {'latest_question_list': latest_question_list}
+
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse(str(question_id) + '번째 투표 상세 화면입니다.')
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    choice = question.choice_set.get(pk=request.POST['choice'])
+
+    choice.votes += 1
+    choice.save()
+
+    return HttpResponseRedirect('result')
 
 
 def result(request, question_id):
-    return HttpResponse(str(question_id) + '번째 투표 결과 화면입니다.')
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'polls/result.html', {'question': question})
+
+
