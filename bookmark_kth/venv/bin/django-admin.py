c0bb91a21e2b8ab782ec841538d152b0f703a0cf
kim tae hyung <>
2019-10-30 10:27:51 +0900

    no message

---
+++ bookmark_kth/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kimtaehyung/django_web_programming/bookmark_kth/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
